4aa020f7f21d5fb74f55d05c15c12e2ccdc9ca26
ekemlage <emilykemlage@gmail.com>
2023-07-10 18:35:03 -0500

    added get element by id to missionTarget in scriptHelper to fix one test failure

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,7 +2,7 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    missionTarget.innerHTML = 
+    document.getElementById("missionTarget").innerHTML = 
     `<h2>Mission Destination</h2>
      <ol>
         <li>Name: ${name}</li>
